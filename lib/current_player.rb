8eaa0b6e0f1acfc5ffb489924535572f8e8f9681
Hunter Sapienza <sapienzahunter@gmail.com>
2018-05-20 22:22:49 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -6,17 +6,17 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-counter = 0
-def turn_count
-  display_board.each do |i|
-    if i == "X"
-      counter += 1
-    elsif i == "O"
-      counter += 1
-    else
-      counter += 0
-    end
-end
+# counter = 0
+# def turn_count
+#   display_board.each do |i|
+#     if i == "X"
+#       counter += 1
+#     elsif i == "O"
+#       counter += 1
+#     else
+#       counter += 0
+#     end
+# end
 
 # def current_player
 #

0d83e7cf8884d72f992c9f168fba94b764c8f646
Hunter Sapienza <sapienzahunter@gmail.com>
2018-05-20 22:20:00 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,13 @@
+counter = 0
 def turn_count
-
+  display_board.each do |i|
+    if i = "X"
+      counter += 1
+    elsif i = "O"
+      counter += 1
+    else
+      counter += 0
+  end
 end
 
 def current_player

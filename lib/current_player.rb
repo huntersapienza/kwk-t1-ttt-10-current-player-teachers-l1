4f1dbdfc1110131313021266038400318004653d
Hunter Sapienza <sapienzahunter@gmail.com>
2018-05-20 22:35:54 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,11 +1,11 @@
-# board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-# def display_board(board)
-#   puts " #{board[0]} | #{board[1]} | #{board[2]} "
-#   puts "-----------"
-#   puts " #{board[3]} | #{board[4]} | #{board[5]} "
-#   puts "-----------"
-#   puts " #{board[6]} | #{board[7]} | #{board[8]} "
-# end
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
 
 def turn_count(display_board)
   counter = 0
